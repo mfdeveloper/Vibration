b0f1b9293abce4bed3d1ecc0760c6fb33cb9b737
Felipe Ferreira <mfelipeof@gmail.com>
2020-06-10 23:06:51 +0100

    [Read Config] Changed the PlayerPrefs stored enable/disable from a ScriptableObject

--- Runtime/Save/Options.cs
+++ Runtime/Save/Options.cs
@@ -1,5 +1,11 @@
-﻿[System.Serializable]
-public partial struct Options
+﻿using UnityEngine;
+
+/// <summary>
+/// Inherit this to additional attributes to your specific ScriptableObject <br/><br/>
+/// <b>Obs:</b> Add your custom ScriptableObject to VibrationComponent.options
+/// </summary>
+[System.Serializable, CreateAssetMenu(fileName = "ConfigOptions", menuName = "ScriptableObjects/ConfigOptions", order = 1)]
+public class Options : ScriptableObject
 {
-    public bool vibration;
+    public bool vibration = true;
 }

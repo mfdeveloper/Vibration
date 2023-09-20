4bfb77457a25e7363a68fb9cc5ecaed91f6bc678
Felipe <mfelipeof@gmail.com>
2023-09-21 00:12:28 +0100

    [Chore] Unity project with UPM embedded structure
    Also:
    
    [VSCode]
    
    - Migrate to use the new extension "visualstudiotoolsforunity.vstuc"
    - Added a default Unity editor debug configuration to `.vscode/launch.json`
    
    [WebGL]
    
    - Refactoring to a more "vanilla" Javascript approach using a `Vibration` singleton class
    - Added `IsVibrateSupported()` method to main `.jslib`
    - Added `repeat` parameter to `VibrateWithPattern()` method on main `.jslib`
    - Added a package.json file to the root embedded Unity project, to run and test WebGL builds
    
    [Android]
    
    - Added a generic static C# class for `AndroidBuild` to get Android API level
    
    [Unity/C#]
    - Refactorings to better accomplish the scene example script `VibrationExample.cs`
    - Added Editor unit tests in order to test `MobileTimeUnit.cs` class
    - Refactoring on `Samples/VibrationExample` to consider use WebGL implementations as well

---
+++ .github/scripts/functions.sh
@@ -0,0 +1,72 @@
+#!/bin/bash
+
+# USAGE: packageVersion "[PATH]/package.json"
+packageVersion() {
+    local PACKAGE_JSON_FILE=$1
+    VERSION=""
+    while read a b ; do 
+        [ "$a" = '"version":' ] && { b="${b%\"*}" ; VERSION="${b#\"}" ; break ; }
+    done < $PACKAGE_JSON_FILE
+    echo $VERSION
+}
+
+githubActionsOutputs() {
+    CURRENT_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
+    COMMIT_MESSAGE=$(git log -1 --pretty=%B)
+    echo ::set-output name=tag::$CURRENT_TAG
+    echo ::set-output name=commit_message::$COMMIT_MESSAGE
+}
+
+copyPackagesContent() {
+    shopt -s extglob dotglob
+    cp -rvf "Packages/$PKG_NAME/." "$PKG_ROOT"
+    rm -rf ./Packages
+}
+
+commitAndPush() {
+    # Incrementing LAST_RELEASE_TAG+1.
+    # Keep here just to store the history, and if need this to the future/others repositories
+    #
+    # PS: Keep in mind that not always you would like to increment the git tag version (e.g rewriting with force an existent git tag)
+    # [[ "$LAST_RELEASE_TAG" =~ (.*[^0-9])([0-9]+)$ ]] && LAST_RELEASE_TAG="${BASH_REMATCH[1]}$((${BASH_REMATCH[2]} + 1))";
+    
+    RELEASE_VERSION=$(packageVersion "./package.json")
+
+    echo "New version: $RELEASE_VERSION"
+
+    if [[ -d "Samples" ]]; then
+    mv Samples Samples~
+    rm -f Samples.meta
+    fi
+    if [[ -d "Documentation" ]]; then
+    mv Documentation Documentation~
+    rm -f Documentation.meta
+    fi
+    git config --global user.name 'github-bot'
+    git config --global user.email 'github-bot@users.noreply.github.com'
+    git add .
+    git commit --allow-empty -am "$COMMIT_MESSAGE"
+
+    echo $RELEASE_VERSION > VERSION.md~
+    git add VERSION.md~
+    git commit -am "fix: Samples => Samples~ and commit a new version: $RELEASE_VERSION"
+    git push -f -u origin "$PKG_BRANCH"
+}
+
+run() {
+    if [ $1 == "push" ]
+    then
+        commitAndPush
+    elif [ $1 == "movePackagesFolder" ]
+    then
+        copyPackagesContent
+    elif [ $1 == "githubActionsVariables" ]
+    then
+        githubActionsOutputs
+    else
+        echo "[ERROR] INVALID SCRIPT OPERATION"
+        exit 1
+    fi
+}
+
+run $1
---
+++ Packages/com.benoitfreslon.vibration/Runtime/Scripts/Enums/MobileTimeUnit.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace VibrationPlugin.Enums
+{
+   /// <summary>
+   /// A string <i>Enum</i> using custom <b>Enumeration class pattern</b>
+   /// </summary>
+   /// <remarks>
+   /// <b> References </b>
+   /// <ul>
+   ///   <li>
+   ///      <a href="https://josipmisko.com/posts/string-enums-in-c-sharp-everything-you-need-to-know">String Enums in C#: Everything You Need to Know</a>
+   ///   </li>
+   /// </ul>
+   /// </remarks>
+   public class MobileTimeUnit : Enumeration<MobileTimeUnit>
+   {
+      public static MobileTimeUnit Nanoseconds => new(nameof(Nanoseconds), 0 );
+      public static MobileTimeUnit Microseconds => new(nameof(Nanoseconds), 1);
+      public static MobileTimeUnit Milliseconds => new(nameof(Milliseconds), 2);
+      public static MobileTimeUnit Seconds => new(nameof(Seconds), 3 );
+      public static MobileTimeUnit Minutes => new(nameof(Minutes), 4);
+      public static MobileTimeUnit Hours => new(nameof(Hours), 5 );
+      public static MobileTimeUnit Days => new(nameof(Days), 6 );
+
+      protected MobileTimeUnit(string name, int value)
+         : base(name, value)
+      {
+      }
+
+      public virtual int ToMilliseconds(long duration)
+      {
+
+         TimeSpan timeSpan;
+         if (this == Milliseconds)
+         {
+            timeSpan = TimeSpan.FromMilliseconds(duration);
+         } else if (this == Seconds)
+         {
+            timeSpan = TimeSpan.FromSeconds(duration);
+         } else if (this == Minutes)
+         {
+            timeSpan = TimeSpan.FromMinutes(duration);
+         } else if (this == Hours)
+         {
+            timeSpan = TimeSpan.FromHours(duration);
+         } else if (this == Days)
+         {
+            timeSpan = TimeSpan.FromDays(duration);
+         } else {
+            throw new ArgumentException($"The timeunit: '${this}' isn't supported on C# layer");
+         }
+
+         return Convert.ToInt32(timeSpan.TotalMilliseconds);
+      }
+   }
+}

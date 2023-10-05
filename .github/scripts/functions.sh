22b0a7df3a1308a85a7dc22afd9ad674d0ffbcda
Felipe Ferreira <mfelipeof@gmail.com>
2023-10-05 23:22:55 +0100

    [Chore] Published this plugin on Github Packages and OpenUPM registries

--- .github/scripts/functions.sh
+++ .github/scripts/functions.sh
@@ -1,33 +1,142 @@
 #!/bin/bash
+# @file functions
+# @brief CI and local package release "automation" functions.
 
-# USAGE: packageVersion "[PATH]/package.json"
+#
+# @description Parse and return a version value from a "package.json" file
+#
+# @see [shdoc](https://github.com/reconquest/shdoc)
+# @example
+#   packageVersion "<PATH>/package.json"
 packageVersion() {
-    local PACKAGE_JSON_FILE=$1
-    VERSION=""
+    local package_json_file=$1
+    local version=""
     while read a b ; do 
-        [ "$a" = '"version":' ] && { b="${b%\"*}" ; VERSION="${b#\"}" ; break ; }
-    done < $PACKAGE_JSON_FILE
-    echo $VERSION
+        [ "$a" = '"version":' ] && { b="${b%\"*}" ; version="${b#\"}" ; break ; }
+    done < $package_json_file
+    echo $version
+}
+
+# 
+# @description $PKG_ROOT environment variable check. Should be a string
+#              with a root path of the package
+#
+# @see $PKG_ROOT
+checkPkgRoot() {
+    if [ -z "$PKG_ROOT" ]
+    then
+        PKG_ROOT=$1
+        if [ -z "$PKG_ROOT" ]
+        then
+            PKG_ROOT="."
+        fi
+    fi
+}
+
+# 
+# @description Replace .env (default) file content with $PKG_ROOT variable value
+#
+# @see $PKG_ROOT
+fixEnvFile() {
+    # $PKG_ROOT environment variable check
+    checkPkgRoot $1
+
+    # Fix env (default) file with correct value 
+    # for environment variables
+    if [ -f "$PKG_ROOT/.env" ]
+    then
+        echo "[FIX .ENV] Replaced $(cat "$PKG_ROOT/.env") to => REPOSITORY_ROOT=."
+        echo "REPOSITORY_ROOT=." > "$PKG_ROOT/.env"
+    fi
 }
 
 # @description Add github actions state and output variables to be handled on .yml workflow files
 #
-# @see [shdoc](https://github.com/reconquest/shdoc)
+# @see $GITHUB_OUTPUT
 # @see [Deprecating save-state and set-output commands](https://github.blog/changelog/2022-10-11-github-actions-deprecating-save-state-and-set-output-commands)
+# @see [Github Actions: Output parameter](https://docs.github.com/en/actions/using-workflows/workflow-commands-for-github-actions#setting-an-output-parameter)
 githubActionsOutputs() {
+    # PS: CURRENT_TAG and COMMIT_MESSAGE are handled here as "global/environment" variables
     CURRENT_TAG=$(git describe --tags $(git rev-list --tags --max-count=1))
     COMMIT_MESSAGE=$(git log -1 --pretty=%B)
+
+    echo "[GITHUB VARIABLES] Commit => $COMMIT_MESSAGE"
+
     # Use the format {name}={value} instead of ::set-output
-    echo "{tag}={$CURRENT_TAG}" >> $GITHUB_OUTPUT
-    echo "{commit_message}={$COMMIT_MESSAGE}" >> $GITHUB_OUTPUT
+    echo "TAG=$CURRENT_TAG" >> $GITHUB_OUTPUT
+    echo "COMMIT_MESSAGE=$COMMIT_MESSAGE" >> $GITHUB_OUTPUT
 }
 
+# @description Copy all content of the package folder to the ROOT dir
+#
+# @see [shdoc](https://github.com/reconquest/shdoc)
+# @example
+#   # From the root folder, with "Packages/<PACKAGE_NAME>"
+#   copyPackagesContent
+#   # result: Copy "Packages/<PACKAGE_NAME>/*.*" to ROOT/
 copyPackagesContent() {
-    shopt -s extglob dotglob
-    cp -rvf "Packages/$PKG_NAME/." "$PKG_ROOT"
+    #shopt -s extglob dotglob
+
+    # Preserve .env file from $PKG_ROOT
+    # if [ -f "$PKG_ROOT/.env"]
+    # then
+    #     mkdir "$PKG_ROOT/tmp"
+    #     mv "$PKG_ROOT/.env" "$PKG_ROOT/tmp"
+    # fi
+
+    cp -rvf "Packages/$PKG_NAME/." "$PKG_ROOT/"
     rm -rf ./Packages
+
+     # Move .env to $PKG_ROOT
+    # if [ -f "$PKG_ROOT/tmp/.env"]
+    # then
+    #     mv -f "$PKG_ROOT/tmp/.env" "$PKG_ROOT/"
+    #     rm -rf "$PKG_ROOT/tmp"
+    # fi
 }
 
+# TODO: Move this common function to another script file in order to reuse (e.g .github/scripts/common.sh)
+#
+# @description Rename invalid package directories to be "untracked" by game engine, adding a "~" suffix
+#              (e.g "Samples" => "Samples~", "Documentation" => "Documentation~")
+#
+# @see $PKG_ROOT
+# @see [shdoc](https://github.com/reconquest/shdoc)
+renameInvalidDirs() {
+    # $PKG_ROOT environment variable check
+    checkPkgRoot $1
+
+    if [ $PKG_ROOT = "./" ]
+    then
+        echo "[RENAME DIRECTORIES] [Error] The \$PKG_ROOT => '$PKG_ROOT' should be just '.' for current directory."
+        return 1
+    fi
+
+    echo "[RENAME DIRECTORIES] Package Root: $PKG_ROOT/"
+
+    # Rename UPM special directories with suffix "~"
+    if [ -d "$PKG_ROOT/Samples" ] && [ ! -d "$PKG_ROOT/Samples~" ]
+    then
+        mv "$PKG_ROOT/Samples" "$PKG_ROOT/Samples~"
+        rm -f "$PKG_ROOT/Samples.meta"
+
+        echo "[RENAMED] Samples => $PKG_ROOT/Samples~"
+    fi
+    if [ -d "$PKG_ROOT/Documentation" ] && [ ! -d "$PKG_ROOT/Documentation~" ]
+    then
+        mv "$PKG_ROOT/Documentation" "$PKG_ROOT/Documentation~"
+        rm -f "$PKG_ROOT/Documentation.meta"
+
+        echo "[RENAMED] Documentation => $PKG_ROOT/Documentation~"
+    fi
+}
+
+# 
+# @description Commit with a new version of the package and push the $PKG_BRANCH 
+#              new orphan branch (usually "upm" branch)
+#
+# @see $PKG_BRANCH
+# @see renameInvalidDirs()
 commitAndPush() {
     # Incrementing LAST_RELEASE_TAG+1.
     # Keep here just to store the history, and if need this to the future/others repositories
@@ -35,27 +144,118 @@ commitAndPush() {
     # PS: Keep in mind that not always you would like to increment the git tag version (e.g rewriting with force an existent git tag)
     # [[ "$LAST_RELEASE_TAG" =~ (.*[^0-9])([0-9]+)$ ]] && LAST_RELEASE_TAG="${BASH_REMATCH[1]}$((${BASH_REMATCH[2]} + 1))";
     
-    RELEASE_VERSION=$(packageVersion "./package.json")
+    local release_version=$(packageVersion "./package.json")
 
-    echo "New version: $RELEASE_VERSION"
+    echo "[COMMIT AND PUSH] New version: $release_version"
 
-    if [[ -d "Samples" ]]; then
-    mv Samples Samples~
-    rm -f Samples.meta
-    fi
-    if [[ -d "Documentation" ]]; then
-    mv Documentation Documentation~
-    rm -f Documentation.meta
-    fi
+    renameInvalidDirs
+
     git config --global user.name 'github-bot'
     git config --global user.email 'github-bot@users.noreply.github.com'
     git add .
     git commit --allow-empty -am "$COMMIT_MESSAGE"
 
-    echo $RELEASE_VERSION > VERSION.md~
+    echo $release_version > VERSION.md~
+    
+    # .env (default) file path fix
+    fixEnvFile
+
     git add VERSION.md~
-    git commit -am "fix: Samples => Samples~ and commit a new version: $RELEASE_VERSION"
+    git commit -am "fix: Samples => Samples~ and commit a new version: $release_version"
     git push -f -u origin "$PKG_BRANCH"
+}
+
+# TODO: Move this function to another script file (e.g .github/scripts/local.sh)
+# 
+# @description Copy a list of files and dirs from the ROOT to the package folder
+#
+# @arg $1 string A path configured as "$repository_root" local variable to be used as an origin
+#                path to copy content into package dir
+# @arg $2 string A path configured as "$PKG_ROOT" environment variable to be used as 
+#                root path of the package
+#
+# @see $PKG_ROOT
+# @see [Exit the Bash Script if a Certain Condition Occurs](https://itslinuxfoss.com/exit-bash-script-if-certain-condition-occurs)
+# @see [How to Check if a File or Directory Exists in Bash](https://linuxize.com/post/bash-check-if-file-exists)
+copyFilesForPublish() {
+    local repository_root=$1
+
+    if [ -z "$repository_root" ]
+    then
+        echo "[COPY FILES] The parameter \$1 => \$repository_root is required: $repository_root"
+        return 1
+    else
+        if [[ "$repository_root" =~ \.$ ]]
+        then
+            repository_root="$repository_root/"
+        fi
+        echo "[COPY FILES] \$repository_root: $repository_root"
+    fi
+
+    # $PKG_ROOT environment variable check
+    checkPkgRoot $2
+
+    local pkg_root_full_path=$(realpath $PKG_ROOT)
+    if [[ $repository_root == $pkg_root_full_path ]]
+    then
+        echo "[COPY FILES] Cannot copy a directory FROM: \$repository_root => '$repository_root' to \$PKG_ROOT => '$pkg_root_full_path', into itself"
+        return 1
+    fi
+
+    chmod -R 777 "$PKG_ROOT/"
+
+    local files_copy=(README.md README.md.meta LICENSE LICENSE.meta Images Images.meta)
+    for file_name in "${files_copy[@]}"
+    do
+        if [[ -f "$repository_root/$file_name" && ! -f "$PKG_ROOT/$file_name" ]] || [[ -d "$repository_root/$file_name" && ! -d "$PKG_ROOT/$file_name" ]]
+        then
+            cp -rf "$repository_root/$file_name" "$PKG_ROOT/$file_name"
+            echo "[COPY FILES] Copied: $PKG_ROOT/$file_name"
+        fi
+    done
+}
+
+# TODO: Move this function to another script file (e.g .github/scripts/local.sh)
+# TODO: Move common functions dependencies to another script file in order to reuse (e.g .github/scripts/common.sh)
+# 
+# @description Automate all actions required before PUBLISH a package in a remote registry
+#
+# @arg $1 string A path configured as "$repository_root" local variable to be used as an origin
+#                path to copy content into package dir
+# @arg $2 string A path configured as "$PKG_ROOT" environment variable to be used as 
+#                root path of the package
+#
+# @see $PKG_ROOT
+# @see renameInvalidDirs($PKG_ROOT)
+# @see copyFilesForPublish($1)
+localBeforePublish() {
+    local repository_root=$1
+
+    if [ -d $repository_root ] && [[ $repository_root != "./" && $repository_root != "." ]]
+    then
+        # $PKG_ROOT environment variable check
+        checkPkgRoot $2
+
+        renameInvalidDirs $PKG_ROOT
+        copyFilesForPublish $repository_root
+    else
+        echo "[PRE PUBLISH] [Skip] Bypass package preparation because \$1 : \$repository_root => '$repository_root' is invalid"
+    fi
+}
+
+# TODO: Move this function to another script file (e.g .github/scripts/local.sh)
+# 
+# @description PUBLISH a package in a remote registry. Usually used inside of a npm script
+#
+# @arg $1 string Overrides the $PKG_ROOT environment variable with a path to a package
+#
+# @see $PKG_ROOT
+localPublish() {
+    # $PKG_ROOT environment variable check
+    checkPkgRoot $1
+
+    cd $PKG_ROOT
+    npm publish
 }
 
 run() {
@@ -68,10 +268,25 @@ run() {
     elif [ $1 == "githubActionsVariables" ]
     then
         githubActionsOutputs
+    elif [ $1 == "copyFilesForPublish" ]
+    then
+        copyFilesForPublish $2 $3
+    elif [ $1 == "fixEnvFile" ]
+    then
+        fixEnvFile $2
+    elif [ $1 == "renameInvalidDirs" ]
+    then
+        renameInvalidDirs $2
+    elif [ $1 == "localBeforePublish" ]
+    then
+        localBeforePublish $2
+    elif [ $1 == "localPublish" ]
+    then
+        localPublish $2
     else
         echo "[ERROR] INVALID SCRIPT OPERATION"
         exit 1
     fi
 }
 
-run $1
+run $1 $2 $3
--- README.md
+++ README.md
@@ -4,13 +4,15 @@
     <img src="./Images/haptic-logo-color.png">
 </h1>
 
+[![openupm](https://img.shields.io/npm/v/com.benoitfreslon.vibration?label=openupm&registry_uri=https://package.openupm.com)](https://openupm.com/packages/com.benoitfreslon.vibration/)
+
 >**PS:** This is a fork of original Vibration package **[BenoitFreslon/Vibration](https://github.com/BenoitFreslon/Vibration)** to be published in [openupm](https://openupm.com) registry, while the changes aren't merged into it!
 
 Native **free** plugin for Unity for Android, iOS and [WebGL](https://caniuse.com/webgl2) (with some limitations).
 Use custom vibrations/haptics on mobile.
 
 If you like this free plugin, that's be cool if you can buy me a coffee 😀☕️
-Send tips to https://paypal.me/UnityVibrationPlugin
+Send tips to <https://paypal.me/UnityVibrationPlugin>
 
 ## Supported Platforms
 
@@ -20,25 +22,101 @@ Send tips to https://paypal.me/UnityVibrationPlugin
 
 ## Installation
 
+### OpenUPM
+
+Install the [OpenUPM](https://openupm.com) CLI and add the [com.benoitfreslon.vibration](https://openupm.com/packages/com.benoitfreslon.vibration) package
+
+```bash
+
+# Install openupm-cli
+npm install -g openupm-cli
+
+# Go to your Unity project directory
+cd YOUR_UNITY_PROJECT_DIR
+
+# Install package: com.benoitfreslon.vibration
+openupm add com.benoitfreslon.vibration
+
+```
+
+Or alternatively, merge the snippet below to [Packages/manifest.json](https://docs.unity3d.com/Manual/upm-manifestPrj.html) manually
+
+```jsonc
+{
+    // Verifiy which is the latest tag in this repository
+    "dependencies": {
+        "com.benoitfreslon.vibration": "<LATEST_VERSION>"
+    },
+    // If you already have the openupm registry,
+    // only add this package to "scopes" array
+    "scopedRegistries": [
+        {
+            "name": "package.openupm.com",
+            "url": "https://package.openupm.com",
+            "scopes": [
+                "com.benoitfreslon.vibration"
+            ]
+        }
+    ]
+}
+```
+
+### Github package
+
+1. Add a **`$HOME/.upmconfig.toml`** file with the TOKEN to authenticate on registry
+
+    ```toml
+    [npmAuth."https://npm.pkg.github.com/@mfdeveloper"]
+    # Generate the token from your github profile:
+    # https://github.com/settings/tokens
+    _authToken = "<TOKEN-VALUE>"
+    email = "<YOUR_EMAIL>"
+    alwaysAuth = true
+    ```
+
+    > **WARNING:** _GitHub/Gitlab_ Packages registry always requires authentication. For **_private_** and **_public_** packages.
+
+2. Configure the scope **registry** and **dependency** in your Unity project `Packages/manifest.json`
+
+    ```jsonc
+    {
+        "dependencies": {
+            // Verifiy which is the latest tag in this repository
+            "com.benoitfreslon.vibration": "<LATEST_VERSION>"
+        },
+        "scopedRegistries": [
+            {
+                "name": "Github Packages: mfdeveloper",
+                "url": "https://npm.pkg.github.com/@mfdeveloper",
+                "scopes": [
+                    "com.benoitfreslon"
+                ]
+            }
+        ]
+    }
+    ```
+
+### Git dependency
+
 The minimal checked Unity Version is **`2019.3.*`** LTS
 
 Open Package Manager and "Add package from git url..." using next string:
 
-* `https://github.com/BenoitFreslon/Vibration.git#upm`
+- `https://github.com/BenoitFreslon/Vibration.git#upm`
 
 Or use the latest git release/tag:
 
-* `https://github.com/BenoitFreslon/Vibration.git#0.1.0`
+- `https://github.com/BenoitFreslon/Vibration.git#<LATEST_VERSION>`
 
 You also can edit `Packages/manifest.json` manually, just add:
 
-* `"com.benoitfreslon.vibration": "https://github.com/BenoitFreslon/Vibration.git#0.1.0",`
+- `"com.benoitfreslon.vibration": "https://github.com/BenoitFreslon/Vibration.git#<LATEST_VERSION>",`
 
 Or you can simply copy and paste the entire `[upm]` branch content from this repo, to your Unity3D `Packages/com.benoitfreslon.vibration` folder.
 
 ## Getting Started
 
-There are 2 ways of usage this plugin:
+There are 2 ways to use this plugin:
 
 1. Use the `Runtime/VibrationComponent.cs` script attached to a _gameObject_ **(Recommended)**
 
@@ -201,14 +279,73 @@ vibration using haptic engine
 
 `Vibration.VibrateIOS_SelectionChanged();`
 
-### References
+## Development
 
-#### ANDROID
+### Publish (Github Packages)
+
+1. Generate a [Github access token (classic)](https://docs.github.com/en/authentication/keeping-your-account-and-data-secure/managing-your-personal-access-tokens#creating-a-personal-access-token-classic)
+
+2. Authenticate using the generated TOKEN:
+
+   Create a user `$HOME/.npmrc` file
+
+    ```ini
+    # Where: <GITHUB_TOKEN> is the access token (classic) generated in your profile
+    @github:registry=https://npm.pkg.github.com
+    //npm.pkg.github.com/:_authToken=<GITHUB_TOKEN>
+    ```
+
+    Or, use the file `Packages/com.benoitfreslon.vibration/.npmrc` that already exists in this repository:
+
+    ```bash
+    # Export the environment variable "$GITHUB_TOKEN"
+    # the file ".npmrc" inside of this repo will use this variable
+    export GITHUB_TOKEN=<GITHUB_TOKEN>
+    ```
+
+3. Publish the package with [npm publish](https://docs.npmjs.com/cli/v9/commands/npm-publish)
+
+    From [`[upm-package-embedded]`](https://github.com/mfdeveloper/Vibration/tree/upm-package-embedded) git branch
+
+    ```bash
+    # Clone this repo and checkout to branch "[upm-package-embedded]"
+    git clone https://github.com/mfdeveloper/Vibration.git
+    git checkout upm-package-embedded
+
+    # Run the npm scripts
+    npm install
+    npm run publish:package # Publish from repository ROOT path
+
+    cd Packages/com.benoitfreslon.vibration 
+    npm publish # Publish from package path
+    ``````
+
+### Publish (OpenUPM)
+
+1. Bump the version with a new Github release or Git tag
+2. The changes should be reflected in: [https://openupm.com/packages/com.benoitfreslon.vibration](https://openupm.com/packages/com.benoitfreslon.vibration)
+
+    > **See:** [Modifying UPM Package](https://openupm.com/docs/modifying-upm-package.html#modifying-upm-package)
+
+## References
+
+### ANDROID
 
 - [Using Vibrate In Android](https://proandroiddev.com/using-vibrate-in-android-b0e3ef5d5e07)
 - [Android 12: VibratorManager & New Vibration Primitives](https://yggr.medium.com/exploring-android-12-vibratormanager-new-vibration-primitives-e862c95fe938)
 - [Developers Android: VibrationEffect](https://developer.android.com/reference/android/os/VibrationEffect)
 - [Developers Android: VibrationEffect.Composition](https://developer.android.com/reference/android/os/VibrationEffect.Composition)
+
+### OPENUPM
+
+- [Adding UPM Package](https://openupm.com/docs/adding-upm-package.html)
+- [Modifying UPM Package](https://openupm.com/docs/modifying-upm-package.html#modifying-upm-package)
+
+### GITHUB PACKAGES
+
+- [Custom Package with Git Dependencies](https://forum.unity.com/threads/custom-package-with-git-dependencies.628390/)
+- [Using GitHub Packages Registry with Unity Package Manager](https://forum.unity.com/threads/using-github-packages-registry-with-unity-package-manager.861076)
+- [Package manager not displaying all packages in scoped registry](https://forum.unity.com/threads/package-manager-not-displaying-all-packages-in-scoped-registry.791598/#post-5666161)
 
 #### ICONS (Copyright)
 
